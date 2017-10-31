ac47f193554a2d0661f27faff899886b79828c44
anssar <rumyancevandr@yandex.ru>
2017-10-31 16:50:41 +0000

    many features, fix bugs, hot fixes, refactoring, documentation

--- online_tests/face_authentication/core.py
+++ online_tests/face_authentication/core.py
@@ -1,32 +1,58 @@
-'''import dlib
+import dlib
 from skimage import io
 from scipy.spatial import distance
+from PIL import Image
+import os
+import pickle
+
 
 ENOUGH_DISTANCE = 0.55
-sp = dlib.shape_predictor('/home/andrey/shape_predictor_68_face_landmarks.dat')
-facerec = dlib.face_recognition_model_v1('/home/andrey/dlib_face_recognition_resnet_model_v1.dat')
+PATH = 'C:/Users/jasfe/Desktop/django-face-authentication-master/online_tests/'
 
-def findFace(image, detector, sp):
+sp = dlib.shape_predictor(PATH + 'shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1(PATH + 'dlib_face_recognition_resnet_model_v1.dat')
+
+detector = dlib.get_frontal_face_detector()
+
+def get_descriptor(image_name):
+    image = io.imread(image_name)
     dets = detector(image, 1)
-    for k,d in enumerate(dets):
+    shape = None
+    for k, d in enumerate(dets):
         shape = sp(image, d)
 
-    return shape
+    if shape is None:
+        return None
 
-def recognize(userFace, dbFaces):
-    userImage = io.imread(userFace)
-    detector = dlib.get_frontal_face_detector()
-    shape = findFace(userImage, detector, sp)
-    userFaceDescriptor = facerec.compute_face_descriptor(userImage, shape)
+    descriptor = facerec.compute_face_descriptor(image, shape)
+    
+    return descriptor
 
-    distanceToDbFaces = []
-    for dbFace in dbFaces:
-        dbImage = io.imread(dbFace)
-        shape = findFace(dbImage, detector, sp)
-        dbFaceDescriptor = facerec.compute_face_descriptor(dbImage, shape)
-        dist = distance.euclidean(userFaceDescriptor, dbFaceDescriptor)
-        distanceToDbFaces.append(dist)
-    return min(distanceToDbFaces) < ENOUGH_DISTANCE
-'''
-def recognize(a,b):
-    return True
+def recognize(image_name, db_path):
+    user_descriptor = get_descriptor(image_name)
+    if user_descriptor is None:
+        return False
+
+    dir_names = os.listdir(PATH + '/tmp/accepted/')
+    for name in dir_names:
+        if '.desc' not in name:
+            continue
+        cur_descriptor = pickle.load(open(PATH + '/tmp/accepted/' + name, 'rb'))
+
+        dist = distance.euclidean(user_descriptor, cur_descriptor)
+
+        if dist < ENOUGH_DISTANCE:
+            return True
+
+    return False
+
+def save_descriptors():
+    dir_names = os.listdir(PATH + '/tmp/accepted/')
+    for name in dir_names:
+        if '.desc' in name:
+            continue
+        cur_descriptor = get_descriptor(PATH + '/tmp/accepted/' + name)
+        pickle.dump(cur_descriptor, open(PATH + '/tmp/accepted/' + name + '.desc', 'wb'))
+
+if __name__ == '__main__':
+    save_descriptors()
--- online_tests/face_authentication/views.py
+++ online_tests/face_authentication/views.py
@@ -14,11 +14,12 @@ def check(request):
 @csrf_exempt
 def checkPhoto(request):
     photo = base64.b64decode(json.loads(request.POST.get('data'))['image'].split(',')[1].strip())
-    file = open('/tmp/faphoto.png', 'wb')
+    path = 'C:/Users/jasfe/Desktop/django-face-authentication-master/online_tests'
+    file = open(path + '/tmp/faphoto.jpg', 'wb')
     file.write(photo)
     file.close()
     users = []
-    if recognize('/tmp/faphoto.png', []):
+    if recognize(path + '/tmp/faphoto.jpg', []):
         request.session['fatoken'] = 'YES'
         return JsonResponse({'result': 'OK'}, status=200)
     return JsonResponse({'result': 'FAIL'}, status=200)
