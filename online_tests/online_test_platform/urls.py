332117c05f6323638925e3a08246fa23764ac251
anssar <rumyancevandr@yandex.ru>
2017-10-31 17:13:47 +0000

    logout

--- online_tests/online_test_platform/urls.py
+++ online_tests/online_test_platform/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.main),
+    url(r'^logout$', views.logout),
 ]
--- online_tests/online_test_platform/views.py
+++ online_tests/online_test_platform/views.py
@@ -4,3 +4,11 @@ from face_authentication.decorators import FARequired
 @FARequired
 def main(request):
     return render(request, 'main.html')
+
+    
+def logout(request):
+    try:
+        del request.session['fatoken']
+    except:
+        pass
+    return render(request, 'main.html')
